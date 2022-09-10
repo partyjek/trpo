ab05398823e5f5c1d9465bbc985710b0048b6759
ZP-021 ReznikovEV <djs@mail.ru>
2022-09-10 18:45:05 +0700

    Fix some errors

--- main.c
+++ main.c
@@ -4,13 +4,54 @@
 
 using namespace std;
 
-int *localize(string ip_net)
-{
+string localize(string ip_net){
+
+    string delim_addr = ".",ip_addr_str;
+    string delim_mask = "/",ip_mask_str,ip_mask_dig;
+    int pos = ip_net.find(delim_mask);
+    int ip_mask[4],mask = 0,j;
+
+    //Split IP address and netmask
+    ip_addr_str = ip_net.substr(0,pos+1);
+    ip_mask_dig = ip_net.substr(pos+1,ip_net.length());
+
+    //Get correct mask address
+    try {
+        for (int i = 0; i < 4; i++){
+            ip_mask[i] = stoi(ip_mask_dig.substr(0,ip_mask_dig.find(delim_addr)));
+            ip_mask_dig.erase(0,ip_mask_dig.find(delim_addr)+1);
+        }
+    }
+    catch(...){
+        cout << "Netmask contains illegal symbols" << endl;
+        exit(0);
+    }
+    for (int i = 0; i < 4; i++){
+        j = 0;
+        while (ip_mask[i] > 1){
+            mask++;
+            ip_mask[i] -= pow(2,7-j);
+            j++;
+        }
+        mask += ip_mask[i];
+    }
+    for (int i = 1; i < 4; i++){
+        if (ip_mask[i] > ip_mask[i-1]){
+            cout << "Netmask is not correct" << endl;
+            exit(0);
+        }
+    }
+    ip_net = ip_addr_str + to_string(mask);
+    return ip_net;
+}
+
+int *gen_ip_array(string ip_net){
+
     int ip_addr_ar[4],ip_mask,ip_mask_dec,ip_mask_ar[4],i;
     string delim_addr = ".",ip_addr_str,ip_addr_dig;
     string delim_mask = "/",ip_mask_str,ip_mask_dig;
     int pos = ip_net.find(delim_mask);
-    static int ip_addr_mask[8];
+    static int *ip_addr_mask = new int[8];
 
     //Split IP address and netmask
     ip_addr_dig = ip_net.substr(0,pos);
@@ -31,7 +72,7 @@ int *localize(string ip_net)
     }
     catch(...){
         cout << "IP address contains illegal symbols" << endl;
-        //return;
+        exit(0);
     }
 
     //Get correct mask
@@ -40,11 +81,11 @@ int *localize(string ip_net)
     }
     catch(...){
         cout << "Net mask is not integer" << endl;
-        //return;
+        exit(0);
     }
     if (ip_mask < 0 | ip_mask > 32){
         cout << "Net mask is not in range (0,32)" << endl;
-        //return;
+        exit(0);
     }
     ip_mask_dec = 255 * (ip_mask/8);
     for (i = 0; i < ip_mask  - 8 * (ip_mask/8); i++){
@@ -67,9 +108,9 @@ int *localize(string ip_net)
 }
 
 int **dec2bin(int* ip){
-    static int **ip_bin=new int*[8];
-    for (int i=0; i < 8; i++)
-        ip_bin[i]=new int[8];
+    static int **ip_bin = new int*[8];
+    for (int i = 0; i < 8; i++)
+        ip_bin[i] = new int[8];
     for (int i = 0; i < 8; i++){
         for (int j = 0; j < 8; j++){
             if (ip[i] >= pow(2,7-j)){
@@ -84,14 +125,26 @@ int **dec2bin(int* ip){
     return ip_bin;
 }
 
-int main()
-{
+int main(){
+
     string ip_net;
+    int* ip_addr_mask;
+    int** ip_bin;
+
+    //Input IP address and Netmask
     cout << "Input IP/mask (ex. A-AAA.B-BBB.C-CCC.D-DDD/EE" << endl;
     cin>>ip_net;
-    int* ip_addr_mask = localize(ip_net);
-    int** ip_bin;
-//    int  (*ip_bin)[8];
+
+    //Localize IP/mask with many types of input
+    if ((ip_net.substr(ip_net.find('/')+1,ip_net.length())).length() <= 2){
+        ip_addr_mask = gen_ip_array(ip_net);
+    }
+    else{
+        ip_net = localize(ip_net);
+        ip_addr_mask = gen_ip_array(ip_net);
+    }
+
+    //Generate binary IP and Netmask (array[[A],[B],[C],[D],[M1],[M2],[M3],[M4]])
     ip_bin = dec2bin(ip_addr_mask);
 
 //    for (int i = 0; i < 8; i++){
@@ -103,12 +156,14 @@ int main()
         }
         cout << endl << endl;
     }
-//    for (int i = 0; i < 8; i++){
-//        for (int j = 0; j < 8; j++){
-//            cout << ip_bin[i][j];
-//        }
-//        cout << endl << endl;
-//    }
+
+
+    //Delete all arrays
+    delete [] ip_addr_mask;
+    for (int i = 0; i < 8; i++){
+        delete [] ip_bin[i];
+    }
+    delete[] ip_bin;
 
     return 0;
 }
