a86b3dd2121e542c4d668ab98ba52a9934d7bf59
ZP-021 ReznikovEV <djs@mail.ru>
2022-09-10 18:59:45 +0700

    Add function bin2addr

--- main.c
+++ main.c
@@ -160,9 +160,7 @@ int *bin2addr(int** ip_bin){
         for (int j = 0; j < 8; j++){
             net_bcast[i] += (ip_bin[i][j] * ip_bin[i+4][j]) * pow(2,7-j);
         }
-        cout << net_bcast[i] << ".";
     }
-    cout << endl << endl;
     for (int i = 0; i < 4; i++){
         net_bcast[i+4] = net_bcast[i];
         for (int j = 0; j < 8; j++){
@@ -175,14 +173,16 @@ int *bin2addr(int** ip_bin){
 
 int main(){
 
+    int i;
     string ip_net;
     int* ip_addr_mask;
     int* net_bcast;
     int** ip_bin;
 
     //Input IP address and Netmask
-    cout << "Input IP/mask (ex. A-AAA.B-BBB.C-CCC.D-DDD/EE" << endl;
-    cin>>ip_net;
+    cout << "Input IP/mask (ex. A-AAA.B-BBB.C-CCC.D-DDD/EE or A-AAA.B-BBB.C-CCC.D-DDD/xxx.xxx.xxx.xxx" << endl;
+    cin >> ip_net;
+    cout << endl;
 
     //Localize IP/mask with many types of input
     if ((ip_net.substr(ip_net.find('/')+1,ip_net.length())).length() <= 2){
@@ -193,28 +193,43 @@ int main(){
         ip_addr_mask = gen_ip_array(ip_net);
     }
 
+    //Print to user
+    cout << "You input:  " << ip_net << endl;
+    cout << "IP address: ";
+    for (i = 0; i < 3; i++){
+        cout << ip_addr_mask[i] << ".";
+    }
+    cout <<ip_addr_mask[i] << endl;
+    cout << "Netmask:    ";
+    for (i = 4; i < 7; i++){
+        cout <<ip_addr_mask[i] << ".";
+    }
+    cout <<ip_addr_mask[i] << endl;
+
     //Generate binary IP and Netmask (array[[A],[B],[C],[D],[M1],[M2],[M3],[M4]])
     ip_bin = dec2bin(ip_addr_mask);
 
     //Find Net adress and broadcast
     net_bcast = bin2addr(ip_bin);
 
-
-
-//    for (int i = 0; i < 8; i++){
-//        cout << ip[i] << endl;
-//    }
-    for (int i = 0; i < 4; i++){
+    //Print to user
+    cout << "Wildcard:   ";
+    for (i = 8; i < 11; i++){
         cout <<net_bcast[i] << ".";
     }
-    cout << endl;
-    for (int i = 0; i < 4; i++){
-        cout <<net_bcast[i+4] << ".";
+    cout <<net_bcast[i] << endl;
+    cout << "Network:    ";
+    for (i = 0; i < 3; i++){
+        cout <<net_bcast[i] << ".";
     }
+    cout <<net_bcast[i];
     cout << endl;
-    for (int i = 0; i < 4; i++){
-        cout <<net_bcast[i+8] << ".";
+    cout << "Broadcast:  ";
+    for (i = 4; i < 7; i++){
+        cout <<net_bcast[i] << ".";
     }
+    cout <<net_bcast[i];
+    cout << endl;
 
     //Delete all arrays
     delete [] ip_addr_mask;
